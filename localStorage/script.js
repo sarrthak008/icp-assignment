29c73a0e0415e7940a913d745e0a138c07e0dcca
sarrthak008 <sarthaknavale08@gmail.com>
2024-08-02 06:49:11 +0530

    pratice the local storage coccept

---
+++ localStorage/script.js
@@ -0,0 +1,12 @@
+
+let animal = "lion"
+ 
+ localStorage.setItem("animal",animal); // add value in localStorage
+
+ let localAnimal = localStorage.getItem('animal') //get animal value from local storage..
+
+ localStorage.removeItem('animal')//removing item from local storage
+
+ localStorage.clear()//that clear the all localstorage key values
+
+ 
